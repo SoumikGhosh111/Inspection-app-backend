e4252239fa57bf11e285c4892cce1d6f9345b998
Bhavya Jain <157887653+BhavyaJain918@users.noreply.github.com>
2024-12-11 19:07:17 +0530

    Added fetchAllSites method

--- controllers/siteController.js
+++ controllers/siteController.js
@@ -25,6 +25,19 @@ async function createSite(req, res) {
     }
 }
 
+async function fetchAllSites(__, res){ 
+    try{ 
+        const result = await ((Site.find()).populate("products_stored.product_id")); 
 
+        if(!result){ 
+            return res.status(404).json({message: "No Products Found"}); 
+        }
 
-module.exports = {createSite}; 
+        return res.status(200).json({message: "Saved Sites", data: result}); 
+
+    }catch(e){ 
+        return res.status(500).json({message: e.message}); 
+    }
+}
+
+module.exports = {createSite, fetchAllSites}; 
