574ff08a5995050fe19dafd7cdf998ab89de70be
Bhavya Jain <157887653+BhavyaJain918@users.noreply.github.com>
2024-12-12 23:09:49 +0530

    Update siteRoute.js

--- routes/siteRoute.js
+++ routes/siteRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 router = express.Router(); 
 
-const {createSite, fetchAllSites} = require("../controllers/siteController"); 
+const {createSite, fetchAllSites, fetchProducts} = require("../controllers/siteController");  
 
 router.post("/create-site", createSite); 
 router.get("/fetch-all-sites", fetchAllSites);
